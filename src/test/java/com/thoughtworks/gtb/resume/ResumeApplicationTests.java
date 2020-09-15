e3baa024d8f4bd72e88dd4391762ab9cf6013894
yurong <rong.yu@thoughtworks.com>
2020-09-15 11:01:21 +0800

    [yurong] feat: init project

---
+++ src/test/java/com/thoughtworks/gtb/resume/ResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.gtb.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
